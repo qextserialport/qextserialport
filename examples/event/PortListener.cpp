fd0f999e27f3161228f546bc8dea265c908778e8
lstask <devnull@localhost>
2010-01-28 17:43:49 -0800

    * drastically simplify event example

--- examples/event/PortListener.cpp
+++ examples/event/PortListener.cpp
@@ -1,99 +1,43 @@
-/**
- * @file PortListener.cpp
- * @brief PortListener Implementation.
- * @see PortListener.h
- */
 
-
-/*
-==============
-<INIT>
-==============
-*/
-
-#include <QTextStream>
-#include <qextserialport.h>
 #include "PortListener.h"
+#include <QtDebug>
 
-
-/*
-==============
-<CONSTRUCTORS>
-==============
-*/
-
-PortListener::PortListener(QextSerialPort * port, QObject * parent):
-	QObject(parent)
+PortListener::PortListener(const QString & portName)
 {
-	this->port = port;
+    qDebug() << "hi there";
+    this->port = new QextSerialPort(portName, QextSerialPort::EventDriven);
+    port->setBaudRate(BAUD56000);
+    port->setFlowControl(FLOW_OFF);
+    port->setParity(PAR_NONE);
+    port->setDataBits(DATA_8);
+    port->setStopBits(STOP_2);
+
+    if (port->open(QIODevice::ReadWrite) == true) {
+        connect(port, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
+        connect(port, SIGNAL(dsrChanged(bool)), this, SLOT(onDsrChanged(bool)));
+        if (!(port->lineStatus() & LS_DSR))
+            qDebug() << "warning: device is not turned on";
+        qDebug() << "listening for data on" << port->portName();
+    }
+    else {
+        qDebug() << "device failed to open:" << port->errorString();
+    }
 }
 
-
-/*
-==============
-<DESTRUCTOR>
-==============
-*/
-
-
-/*
-==============
-<STATIC>
-==============
-*/
-
-
-/*
-==============
-<SLOTS>
-==============
-*/
-
-void PortListener::receive()
+void PortListener::onReadyRead()
 {
-	char data[1024];
-	QTextStream out(stdout);
-	
-	out << "data received: ";
-	int bytesRead = port->read(data, 1024);
-	data[bytesRead] = '\0';
-	out << data << " (" << bytesRead << " bytes)" << endl;
+    QByteArray bytes;
+    int a = port->bytesAvailable();
+    bytes.resize(a);
+    port->read(bytes.data(), bytes.size());
+    qDebug() << "bytes read:" << bytes.size();
+    qDebug() << "bytes:" << bytes;
 }
 
-void PortListener::reportWritten(qint64 bytes)
+void PortListener::onDsrChanged(bool status)
 {
-	QTextStream out(stdout);
-	
-	out << bytes << " bytes written" << endl;
+    if (status)
+        qDebug() << "device was turned on";
+    else
+        qDebug() << "device was turned off";
 }
-
-void PortListener::reportClose()
-{
-	QTextStream out(stdout);
-	
-	out << "closing port" << endl;	
-}
-
-void PortListener::reportDsr(bool status)
-{
-	QTextStream out(stdout);
-	
-	if (status)
-		out << "device was turned on" << endl;
-	else
-		out << "device was turned off" << endl;
-}
-
-/*
-==============
-<VIRTUAL>
-==============
-*/
-
-
-/*
-==============
-<NON-VIRTUAL>
-==============
-*/
-
--- examples/event/PortListener.h
+++ examples/event/PortListener.h
@@ -1,56 +1,24 @@
-/**
- * @file PortListener.h
- * @brief Port Listener.
- * @see PortListener
- */
+
+
 
 #ifndef PORTLISTENER_H_
 #define PORTLISTENER_H_
 
 #include <QObject>
+#include "qextserialport.h"
 
-class QextSerialPort;
-
-
-/**
- * Port Listener.
- */
 class PortListener : public QObject
 {
-	Q_OBJECT
-	
-	QextSerialPort * port;
+Q_OBJECT
+public:
+    PortListener(const QString & portName);
 
+private:
+    QextSerialPort *port;
 
-	public:
-		/**
-		 * Constructor.
-		 *  @param parent parent object.
-		 */
-		PortListener(QextSerialPort * port, QObject * parent = 0);
-	
-	public slots:
-		/**
-		 * Receive data from serial port.
-		 */
-		void receive();
-		
-		/**
-		 * Report written bytes.
-		 * 	@param bytes number of written bytes.
-		 */
-		void reportWritten(qint64 bytes);
-		
-		/**
-		 * Report port closing.
-		 */
-		void reportClose();
-		
-		/**
-		 * Report DSR line.
-		 * 	@param status line status.
-		 */
-		void reportDsr(bool status);
+private slots:
+    void onReadyRead();
+    void onDsrChanged(bool status);
 
 };
 
